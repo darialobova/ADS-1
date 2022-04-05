8e4f7d5e7fa7cf506e43d6f7a90ad0e4b5b85441
Лобова Дарья Сергеевна <94217366+darialobova@users.noreply.github.com>
2022-04-05 23:09:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,41 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  for (int x = 2; x*x <= (value); x++) {
+      if (value % x == 0) {
+        return false;
+      }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int k = 0;
+  uint64_t x = 1;
+  while (k != n) {
+    x++;
+    if (checkPrime(x)) {
+      k++;
+    }
+  }
+  return x;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  int z = value;
+  while (true) {
+    z++;
+    if (checkPrime(z))
+      reurn z;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t sum = 0;
+  for (int x = 2; x < hbound; x++) {
+    if (checkPrime(x)) {
+      sum += x;
+    }
+  }
+  return sum;
 }
